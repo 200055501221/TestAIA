7cf99eb4d49f3c710268a005aa0e60c39d39c62a
200055501221 <maleeshanimanthi2000@gmail.com>
2025-07-02 15:15:50 +0530

    Saving my local work before fetching

---
+++ src/main/java/aiatest/function/DatabaseUtil.java
@@ -0,0 +1,17 @@
+// package aiatest.function;
+
+// import java.sql.DriverManager;
+// import java.sql.SQLException;
+// import java.sql.Connection;
+
+// public class DatabaseUtil {
+//      private static final String URL = "jdbc:mysql://localhost:3306/azuredb";
+//     private static final String USER = "root";
+//     private static final String PASSWORD = "Malee@2000";
+
+//     public static Connection getConnection() throws SQLException {
+//         return DriverManager.getConnection(URL, USER, PASSWORD);
+//     }
+// }
+
+
